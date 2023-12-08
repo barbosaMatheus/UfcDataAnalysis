a38631a278f01a8750f7f21b4035a1345ed13683
Matheus <matheus@ravioli.com>
2023-12-07 19:38:35 -0600

    cleanup and added new column fighter id

--- data_analysis.py
+++ data_analysis.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from os.path import isfile
 
 MONTHS = {"JAN": 1, "FEB": 2, "MAR": 3, "APR": 4, "MAY": 5,
                     "JUN": 6, "JUL": 7, "AUG": 8, "SEP": 9, "OCT": 10,
@@ -33,30 +34,34 @@ def decode_date(df, cname="DOB"):
 
 if __name__ == "__main__":
     # data from: https://www.kaggle.com/datasets/rajeevw/ufcdata?select=preprocessed_data.csv
-    fighters = pd.read_csv("raw_fighter_details.csv")
-    bouts = pd.read_csv("raw_total_fight_data.csv", sep=';')
+    fighters_fname = "fighters.csv"
+    bouts_fname = "bouts.csv"
+    if not isfile(fighters_fname):
+        fighters = pd.read_csv("raw_fighter_details.csv")
+        # cleanup fighters df
+        # show_col_dtypes(fighters)
+        fighters["Stance"] = fighters["Stance"].fillna("unkown_stance")
+        fighters["Stance"] = fighters["Stance"].astype(str)
+        stance = pd.get_dummies(fighters["Stance"])
+        stance.columns = ["open_stance", "orthodox_stance", "sideways_stance",
+                        "southpaw_stance", "switch_stance", "unkown_stance"]
+        stance = stance.astype(np.float32)
+        fighters.drop(["Stance"], axis=1, inplace=True)
+        fighters = fighters.join(stance)
+        fighters["Weight"] = fighters["Weight"].str.split(" ", n=1, expand=True)[0].astype(np.float32)
+        fighters["Height"] = height_to_inches(fighters)
+        rem_last = ["Reach", "Str_Acc", "Str_Def", "TD_Acc", "TD_Def"]
+        for col in rem_last:
+            fighters[col] = remove_last(fighters, col).astype(np.float32)
+        fighters = decode_date(fighters)
+        fighters.insert(0, "fighter_id", range(fighters.shape[0]))
+        print(fighters)
+        fighters.to_csv(fighters_fname)
+    else:
+        fighters = pd.read_csv(fighters_fname)
+    if not isfile(bouts_fname):
+        bouts = pd.read_csv("raw_total_fight_data.csv", sep=';')
 
-    # cleanup fighters df
-    fighters["Stance"] = fighters["Stance"].fillna("unkown_stance")
-    fighters["Stance"] = fighters["Stance"].astype(str)
-    stance = pd.get_dummies(fighters["Stance"])
-    stance.columns = ["open_stance", "orthodox_stance", "sideways_stance",
-                      "southpaw_stance", "switch_stance", "unkown_stance"]
-    stance = stance.astype(np.float32)
-    fighters.drop(["Stance"], axis=1, inplace=True)
-    fighters = fighters.join(stance)
-    fighters["Weight"] = fighters["Weight"].str.split(" ", n=1, expand=True)[0].astype(np.float32)
-    fighters["Height"] = height_to_inches(fighters)
-    rem_last = ["Reach", "Str_Acc", "Str_Def", "TD_Acc", "TD_Def"]
-    for col in rem_last:
-        fighters[col] = remove_last(fighters, col).astype(np.float32)
-    fighters = decode_date(fighters)
-    print(fighters)
-
-    # cleanup bouts dataframe
-
-    print("Fighter Details:")
-    show_col_dtypes(fighters)
-
-    # print("Bout Details:")
-    # show_col_dtypes(bouts)
+        # cleanup bouts dataframe
+        print("Bout Details:")
+        show_col_dtypes(bouts)
